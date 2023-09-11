a4f09340aaee255c34fa120bb1040dc6fc89f858
Alfie Collins <alfiecollins44@gmail.com>
2023-09-11 19:56:34 +0100

    initial stuff

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//Canvas
+const canvas = document.getElementById('canvas');
+canvas.style.display = 'none';
+
+//Opening Section
+const openingSection = document.querySelector('opening-section');
+
+//Start Button
+const startButton = document.getElementById('start-button');
+window.onload = () => {
+    startButton.onclick = () => {
+    openingSection.style.display = 'none';
+    canvas.style.display = '';
+    };
+  };
