468fd549c4856936ec4505de253d952ef887c6ca
Alfie Collins <alfiecollins44@gmail.com>
2023-09-12 23:18:18 +0100

    stopping the animation loop after pressing homepage

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 let background = new Image();
 let backgroundX = 0;
 background.src = "./images/game.jpg";
+let homepaged = false;
 
 // Canvas
 const canvas = document.getElementById('canvas');
@@ -11,47 +12,64 @@ canvas.style.display = 'none';
 // Opening Section
 const openingSection = document.querySelector('.opening-section');
 
-// Start Button
-window.onload = () => {
-const startButton = document.getElementById('start-button');
-startButton.onclick = () => {
-  opening.pause();
-  openingSection.style.display = 'none';
-  canvas.style.display = '';
-  audioControls.style.display = '';
-  playNextRandomSong();
-  startGame();
+// Homepage Button
+const homeButton = document.querySelector('.homepage');
+homeButton.onclick = () => {
+  pauseGameMusic();
+  openingSection.style.display = '';
+  canvas.style.display = 'none';
+  audioControls.style.display = 'none';
+  currentShip.x = canvas.width / 2;
+  currentShip.y = canvas.height / 1.25;
+  backgroundX = 0;
+  cancelAnimationFrame(animationID); // Stop the animation loop
 }
+
+// Start Button
+let animationID; // Store the animation ID
+window.onload = () => {
+  const startButton = document.getElementById('start-button');
+  startButton.onclick = () => {
+    opening.pause();
+    openingSection.style.display = 'none';
+    canvas.style.display = '';
+    audioControls.style.display = '';
+    playNextRandomSong();
+    startGame();
+  };
 };
 
 function startGame() {
   currentGame = new Game();
 
-    // Instantiate a new ship
-    currentShip = new Ship();
-    currentShip.drawShip();
+  // Instantiate a new ship
+  currentShip = new Ship();
+  currentShip.drawShip();
 
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height); // draw background image
+  // Clear any previous animation loop
+  cancelAnimationFrame(animationID);
 
-  requestAnimationFrame(updateCanvas);
+  // Start the animation loop
+  animationID = requestAnimationFrame(updateCanvas);
 }
 
 function updateCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 
   // Scroll the background to the left
-  backgroundX -= 0.3; 
+  backgroundX -= 0.3;
   // Reset the background position if it goes beyond the canvas width
   if (backgroundX <= -canvas.width) {
     backgroundX = 0;
   }
 
-   // Draw background image
-   ctx.drawImage(background, backgroundX, 0, canvas.width, canvas.height);
+  // Draw background image
+  ctx.drawImage(background, backgroundX, 0, canvas.width, canvas.height);
   // Draw the second copy of the background image to create the continuous scroll
   ctx.drawImage(background, backgroundX + canvas.width, 0, canvas.width, canvas.height);
 
   currentShip.drawShip(); // redraw the ship at its current position
 
-  requestAnimationFrame(updateCanvas);
+  // Continue the animation loop
+  animationID = requestAnimationFrame(updateCanvas);
 }
