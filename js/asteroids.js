68161e2b4436520724f6c838e49e0bf9c99370e6
Alfie Collins <alfiecollins44@gmail.com>
2023-09-20 17:50:50 +0100

    adding explosions

--- js/asteroids.js
+++ js/asteroids.js
@@ -4,15 +4,33 @@ class Obstacle {
         this.y = y;
         this.width = 50;
         this.height = 70;
-        this.img = './images/asteroid.png'
+        this.img = './images/asteroid.png';
         this.destroyed = false;
+        this.wasHit = false; // Track if the obstacle was hit
+        this.explosionFrames = 10; // Number of frames to display the explosion
+        this.currentExplosionFrame = 0; // Current explosion frame
+        this.explosionImage = new Image();
+        this.explosionImage.src = './images/explosion.png';
     }
 
-    drawObstacle(){
+    drawObstacle() {
+      if (this.wasHit && this.currentExplosionFrame < this.explosionFrames) {
+        // Display the explosion image
+        ctx.drawImage(
+          this.explosionImage,
+          this.x,
+          this.y,
+          this.width,
+          this.height
+        );
+        this.currentExplosionFrame++;
+      } else {
+        // Draw the asteroid image
         const obstacleImg = new Image();
         obstacleImg.src = this.img;
         ctx.drawImage(obstacleImg, this.x, this.y, this.width, this.height);
       }
+    }
 
       collidesWith(x, y) {
         return (
@@ -25,7 +43,7 @@ class Obstacle {
 
       destroy() {
         this.destroyed = true;
+        this.wasHit = true; // Mark the obstacle as hit
       }
-    
 
     }
